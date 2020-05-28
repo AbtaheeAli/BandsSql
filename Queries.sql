d3c6cdbcc7db91aa39d28384af59154b8b60d6c6
Abtahee Ali <abtaheeali@gmail.com>
2020-05-28 15:55:05 -0400

    Updated IsSigned for band to false and updated it again to true

--- Queries.sql
+++ Queries.sql
@@ -1,10 +1,15 @@
-CREATE TABLE "Albums" ("Id" SERIAL PRIMARY KEY, "Title" TEXT, "IsExplicit" BOOLEAN, "ReleaseDate" TIMESTAMP);
+CREATE TABLE "Albums" ("Id" SERIAL PRIMARY KEY, "Title" TEXT, "IsExplicit" BOOLEAN, "ReleaseDate" DATE);
 
 CREATE TABLE "Band" ("Id" SERIAL PRIMARY KEY, "Name" TEXT, "CountryOfOrigin" TEXT, "NumberOfMembers" INT, "Website" TEXT, "Style" TEXT, "IsSigned" BOOLEAN, "ContactName" TEXT, "ContactPhoneNumber" TEXT);
 
 ALTER TABLE "Albums" ADD COLUMN "BandId" INTEGER NULL REFERENCES "Bands" ("Id");
 
-INSERT INTO "Band" ("Name", "CountryOfOrigin", "NumberOfMembers", "Website", "Style", "IsSigned", "ContactName", "ContactPhoneNumber") VALUES ('Migos', 'USA', '3', 'Migos.com', 'Hip-Hop', 'True', 'Tahee', '727-299-9999');
+INSERT INTO "Bands" ("Name", "CountryOfOrigin", "NumberOfMembers", "Website", "Style", "IsSigned", "ContactName", "ContactPhoneNumber") VALUES ('Migos', 'USA', '3', 'Migos.com', 'Hip-Hop', 'True', 'Tahee', '727-299-9999');
 
 SELECT * FROM "Band";
 
+INSERT INTO "Albums" ("Title", "IsExplicit", "ReleaseDate", "BandId") VALUES ('Culture II', 'True', '2016-02-01', '1');
+
+UPDATE "Bands" SET "IsSigned" = false where "Name" = 'Migos';
+
+UPDATE "Bands" SET "IsSigned" = true where "Name" = 'Migos';
